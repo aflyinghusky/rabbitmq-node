db8551769fade219f6f2b6c265a34940adc7e103
aflyingHusky <canhquanvhit@gmail.com>
2025-02-04 14:07:57 +0700

    update cdr-helper

--- cdr-helper.js
+++ cdr-helper.js
@@ -2,35 +2,47 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const sampleCDRs = (date) => [
+function ObjectId() {
+  const timestamp = Math.floor(Date.now() / 1000).toString(16).padStart(8, '0');
+  const machineId = Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0');
+  const processId = process.pid.toString(16).padStart(4, '0');
+  const increment = Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0');
+
+  return timestamp + machineId + processId + increment;
+}
+
+const sampleCDRs = (date, _id = ObjectId()) => [
   {
+    _id,
+    "score": 1,
+    "tags": [],
+    "caller": "842367109722",
+    "createdAt": date,
+    "state": "NOANSWER",
+  },
+  {
+    _id,
     "score": 1,
     "tags": [],
     "duration": 0,
     "state": "NOANSWER",
     "caller": "842367109722",
     "createdAt": date,
-    "actionCode": "NOANSWER"
   },
   {
-    "score": 1,
-    "tags": [],
-    "duration": 0,
-    "state": "USER_HANGUP",
-    "caller": "842367109722",
-    "createdAt": date,
-    "actionCode": "CU"
-  },
-  {
+    _id,
     "score": 1,
     "tags": [],
     "duration": 0,
     "state": "CHANUNAVAIL",
     "caller": "842367109722",
     "createdAt": date,
-    "actionCode": "CU",
   },
 ];
-exports.getSampleCdr = (date) => {
+const getSampleCdr = (date) => {
   return sampleCDRs(date)[getRandomInt(sampleCDRs.length)];
 };
+
+module.exports = {
+  getSampleCdr,
+}
--- sender.js
+++ sender.js
@@ -2,21 +2,31 @@
 const amqp = require('amqplib/callback_api')
 
 // Create connection
-amqp.connect('amqp://localhost', (err, conn) => {
+amqp.connect('amqp://user:password@rabbitmq-dev.vnlp.ai/quan-dev', (err, conn) => {
   // Create channel
   conn.createChannel((err, ch) => {
     // Name of the queue
     const q = 'task_queue_durable'
     // Write a message
     const msg = process.argv.slice(2).join(' ') || "Hello World!"
+    const newOptions = {
+      durable: true,
+      exchangeType: 'direct',
+    }
+    ch.assertExchange(q, 'direct', {
+      durable: newOptions.durable,
+      arguments: {
+        'x-message-ttl': 15000,
+      },
+    });
 
     // Declare the queue
-    ch.assertQueue(q, { durable: true }) // { durable: true } ensures that the message will still be redelivered even if RabbitMQ service is turned off/restarted
+    // ch.assertQueue(q, { durable: true }) // { durable: true } ensures that the message will still be redelivered even if RabbitMQ service is turned off/restarted
 
     // Send message to the queue
 
     for (let i = 0; i < 200; i++) {
-      ch.sendToQueue(q, new Buffer(`${msg} ${i}`), {persistent: true}) // {persistent: true} saves the message to disk/cache
+      ch.sendToQueue(q, new Buffer(`${msg} ${i}`), { persistent: true }) // {persistent: true} saves the message to disk/cache
       console.log(` {x} Sent '${msg}' ${i}`)
     }
 
